925464f23d58e8d6e5a5a4c82f8a2e69a44370b9
Perez271 <104815561+Perez271@users.noreply.github.com>
2023-02-02 10:54:19 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+document.querySelector(".info").addEventListener('click', function() {
+    document.querySelector(".time").scrollIntoView({ behavior: 'smooth' });
+});
+
+
+document.querySelector(".linker").addEventListener('click', function() {
+    window.location.href = "serivices.html";
+});
+
+document.querySelector(".btn").addEventListener('click', function() {
+    window.location.href = "about us.html";
+});
+
+document.querySelector(".ser").addEventListener('click', function() {
+    window.location.href = "cont.html";
+});
